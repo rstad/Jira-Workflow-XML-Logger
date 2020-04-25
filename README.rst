99b2bc08de38a532eb4430ab714a75ac8991f041
Russell Stadler <3325739+rstad@users.noreply.github.com>
2020-04-25 08:52:58 -0700

    packaged up for initial commit

---
+++ README.rst
@@ -0,0 +1,80 @@
+========================
+Jira Workflow XML Logger
+========================
+
+Example / Demo
+--------------
+
+I started up a Jira trial locally, made some config changes over the course of a few days, and pointed this script at it periodically.  The results can be found here: `https://github.com/rstad/jira-workflow-log-example <https://github.com/rstad/jira-workflow-log-example>`_
+
+Requirements
+------------
+
+- Python 3.6+
+- credentials for a user that has sufficient privileges to retrieve workflows from Jira
+- a repo you can access for committing changes to
+
+Usage
+-----
+
+For now I'm just running this as a cron job but I'm sure you could adapt it for other use.
+
+Manual steps:
+
+#. ``git clone`` this repo and ``cd`` into the directory.
+#. ``cp .env.example .env``
+#. edit ``.env`` to have the appropriate values
+#. ``python3 -m venv env``
+#. ``source env/bin/activate``
+#. ``pip install -r requirements.txt``
+#. ``python workflowlog.py``
+
+
+------------
+
+::
+
+ ❯ python workflowlog.py -h
+ usage: workflowlog.py [-h] [--firstrun] [--nocleanup]
+ 
+ Script to capture changes to workflows in Jira
+ 
+ optional arguments:
+   -h, --help   show this help message and exit
+   --firstrun   Gets ALL workflows and attempts to make initial commit to an empty repo
+   --nocleanup  prevents deleting work dir after run
+
+------------
+
+Background
+----------
+
+The idea for this script came up when I was talking to somebody about how we could better track who made changes to a workflow, and what changes they had made.
+
+The actions we resolved to take from that meeting were to essentially ask people to remember to save backup copies of workflows when publishing updates.  This sounded to me like a good opportunity to automate.
+
+Approach
+--------
+It's possible to get a list of all the workflows that Jira has, in a giant JSON blob.  It was not immediately obvious to me how to tell which ones are active or inactive, but for the purposes of simply logging changes to workflows, that doesn't matter too much.
+
+What the big list has that we do care about is:
+
+#. a ``lastModifiedDate``
+#. a ``lastModifiedUser``
+#. a uniquely identifying ``name``
+
+With this, the next question is, how do I get the workflow as an XML file, for the purposes of storing and performing ``diff`` against later?
+
+It `turns out <https://community.atlassian.com/t5/Answers-Developer-Questions/How-to-get-all-workflow-steps-through-rest-api/qaq-p/542091>`_ that this is pretty straightforward by performing the appropriate series of requests to get authenticated, and then simply trying to download it by name using:
+
+``$JIRA_URL/secure/admin/workflows/ViewWorkflowXml.jspa?workflowMode=live&workflowName=My+Workflow+Name``
+
+So far, we've now got a list of every workflow, the XML for that workflow, who last modified that workflow (which is also in the XML), and when the workflow was last updated.
+
+To put it all together, we use this script which will:
+
+#. Retrieve a list of all workflows
+#. Check for those that were last updated recently (e.g. since the last time the script was run)
+#. For each workflow identified, download the XML
+#. Add to a git repo and commit the changes
+
---
+++ workflowlog.py
@@ -0,0 +1,108 @@
+import os # to read environment variables and deal with directory stuff
+import sys # for sys.exit only?
+import argparse # for taking arguments
+import httpx # to make http requests to JIRA and Slack
+import json # for working with responses from Jira, etc.
+import parsedatetime # for parsing relative and human readable time descriptions
+from git import Repo,Git # for git operations
+from shutil import rmtree # for deleting work dir
+from urllib.parse import quote_plus # converting workflow names to URL properly
+from datetime import datetime,timedelta # working with dates and comparing them
+from pprint import pprint # for pretty printing while debugging
+from dotenv import load_dotenv # to set environment variables, mainly
+load_dotenv()
+
+parser = argparse.ArgumentParser(description="Script to capture changes to workflows in Jira")
+parser.add_argument('--firstrun',help="Gets ALL workflows and attempts to make initial commit",action='store_true')
+parser.add_argument('--nocleanup',help="prevents deleting work dir after run",action='store_true')
+args = parser.parse_args()
+
+git_ssh_command = 'ssh -i %s' % os.getenv("gitkeypath")
+
+def setupWorkdir():
+    if os.path.isdir("./work"):
+        rmtree("./work")
+    if (args.firstrun):
+        os.makedirs("work")
+        repo = Repo.init("./work",env={"GIT_SSH_COMMAND":git_ssh_command})
+    else:
+        repo = Repo.clone_from(os.getenv("gitremote"),"./work",depth=1,env={"GIT_SSH_COMMAND":git_ssh_command})
+    return repo
+
+def commitChanges(repo):
+    repo.git.add(".")
+    if (args.firstrun):
+        repo.index.commit("initial commit")
+        origin = repo.create_remote('origin',os.getenv("gitremote"))
+        repo.create_head('master')
+        origin.push('master',env={"GIT_SSH_COMMAND":git_ssh_command})
+    elif (len(repo.index.diff("HEAD"))>0): # there exists changes
+        repo.index.commit(str(datetime.now()))
+        origin = repo.remote('origin')
+        origin.push()
+
+def cleanup():
+    if (args.nocleanup):
+        return
+    rmtree("./work")
+
+def getWorkflows():
+    jira_url_base = os.getenv("jirabaseurl")
+    jira_auth_user = os.getenv("jirauser")
+    jira_auth_password = os.getenv("jirapass")
+    workflows=[]
+    with httpx.Client(auth=(jira_auth_user,jira_auth_password)) as jiraclient:
+        workflowList = jiraclient.get(jira_url_base + 'rest/api/2/workflow',timeout=10.0)
+        workflows = json.loads(workflowList.text)
+
+    updated_workflows = []
+    current_time = datetime.now()
+    timeparser = parsedatetime.Calendar()
+    # this is super gross, and is largely avoided if you use the jira config property to disable relativized dates
+    for wf in workflows:
+        modified_datetime = datetime.now()
+        if (args.firstrun):
+            updated_workflows.append(wf['name'])
+            continue
+        elif (wf['default']):
+            # there is only one default workflow, and it is read only
+            continue
+        elif ("lastModifiedDate" not in wf):
+            # going to skip workflows that have never been modified
+            continue
+        elif ("now" in wf['lastModifiedDate']): # the "Just now" case
+            modified_datetime = datetime.now()-timedelta(minutes=1)
+        elif ("ago" in wf['lastModifiedDate']): # the "X minutes ago" and "Y hours ago" cases
+            if ("minute" in wf['lastModifiedDate']):
+                timestruct,status = timeparser.parse(wf['lastModifiedDate'])
+                modified_datetime = datetime(*timestruct[:6])
+            elif ("hour" in wf['lastModifiedDate']):
+                timestruct,status = timeparser.parse(wf['lastModifiedDate'])
+                modified_datetime = datetime(*timestruct[:6])
+        elif ("Yesterday" in wf['lastModifiedDate']): # Yesterday H:MM PM case
+            timestruct,status = timeparser.parse(wf['lastModifiedDate'])
+            modified_datetime = datetime(*timestruct[:6])
+        else:
+            modified_datetime = datetime.strptime(wf['lastModifiedDate'], '%d/%b/%y %I:%M %p')
+        delta = current_time - modified_datetime
+        if (delta.days < 1):
+            updated_workflows.append(wf['name'])
+
+    jiraheaders = {"X-Atlassian-Token" : "no-check"}
+    with httpx.Client(auth=(jira_auth_user,jira_auth_password),headers=jiraheaders) as jirasudoclient:
+        myself = jirasudoclient.get(jira_url_base + 'rest/api/2/myself')
+        websudo_headers = {"content-type":"application/x-www-form-urlencoded"}
+        for wf in updated_workflows:
+            websudo_data = {'webSudoPassword':jira_auth_password,'webSudoDestination':"/secure/admin/workflows/ViewWorkflowXml.jspa?workflowMode=live&workflowName="+quote_plus(wf)}
+            websudo = jirasudoclient.post(jira_url_base + 'secure/admin/WebSudoAuthenticate.jspa',headers=websudo_headers,data=websudo_data)
+            with open("./work/"+wf.replace("/","_")+".xml",'w') as file:
+                file.write(websudo.text)
+
+if __name__ == '__main__':
+    try:
+        repo = setupWorkdir()
+        getWorkflows()
+        commitChanges(repo)
+        cleanup()
+    except KeyboardInterrupt:
+        print('keyboard interrupt')
